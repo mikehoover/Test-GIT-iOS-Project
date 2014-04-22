e0e710704f3dc80f9eb835823662c4417841528d
mikehoover <mikehoover@sc.rr.com>
2014-04-22 19:48:06 -0400

    042114_03

---
+++ Git Test/GTNewClass.h
@@ -0,0 +1,13 @@
+//
+//  GTNewClass.h
+//  Git Test
+//
+//  Created by Mike's iMac on 4/22/14.
+//  Copyright (c) 2014 Mike Hoover. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GTNewClass : NSObject
+
+@end
---
+++ Git Test/GTNewClass.m
@@ -0,0 +1,13 @@
+//
+//  GTNewClass.m
+//  Git Test
+//
+//  Created by Mike's iMac on 4/22/14.
+//  Copyright (c) 2014 Mike Hoover. All rights reserved.
+//
+
+#import "GTNewClass.h"
+
+@implementation GTNewClass
+
+@end
